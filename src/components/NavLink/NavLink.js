da3b554a4e44d33796abacbc6fe93778be89f132
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-13 20:27:57 +0300

    feat: добавить компонент NavLink

---
+++ src/components/NavLink/NavLink.js
@@ -0,0 +1,10 @@
+function NavLink({}) {
+
+	return(
+		<div className="navtab">
+			NAVLINK
+		</div>
+	);
+};
+
+export default NavLink;
--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,8 +1,13 @@
+import NavLink from "../NavLink/NavLink";
+
 function NavTab({}) {
 
 	return(
 		<div className="navtab">
 			NAVTAB
+			<NavLink/>
+			<NavLink/>
+			<NavLink/>
 		</div>
 	);
 };
