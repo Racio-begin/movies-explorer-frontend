dc621007a6d12bca615a3fad9ac4567fa5a0b3c3
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-17 19:23:55 +0300

    feat: сверстать секцию Promo, оттипографировать текст в компоненте AboutProjects, сверстать его первую часть

--- src/components/AboutProjects/AboutProjects.js
+++ src/components/AboutProjects/AboutProjects.js
@@ -8,14 +8,15 @@ function AboutProjects({ }) {
 			<div className="about-projects__description">
 
 				<div className="about-projects__description_container">
-					<div className="about-projects__description_title">Дипломный проект включал 5 этапов</div>
-					<div className="about-projects__description_content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</div>
+					<div className="about-projects__description_title">Дипломный проект включал 5&nbsp;этапов</div>
+					<div className="about-projects__description_content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</div>
 				</div>
 
 				<div className="about-projects__description_container">
-					<div className="about-projects__description_title">На выполнение диплома ушло 5 недель</div>
-					<div className="about-projects__description_content">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</div>
+					<div className="about-projects__description_title">На&nbsp;выполнение диплома ушло 5&nbsp;недель</div>
+					<div className="about-projects__description_content">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</div>
 				</div>
+
 			</div>
 
 			<div className="about-projects__study-time"></div>
