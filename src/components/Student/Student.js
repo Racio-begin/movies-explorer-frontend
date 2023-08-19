675f089b99e945f9b03791d958f9c63232a185ca
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-19 14:17:02 +0300

    fix: оптимизировать применение шрифтов, сверстать компонент Student

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,22 @@
 import './Student.css';
-import avatar from '../../images/user/my-avatar_wev-developer.jpg'
+import avatar from '../../images/user/my-avatar_wev-developer_2.jpg'
 
 function Student() {
 
 	return (
 		<div className="student">
-			<div className="student__info">
-				<h3 className="student__title">Никита</h3>
-				<p className="student__subtitle">Фронтенд-разработчик, 30&nbsp;лет</p>
-				<p className="student__about">Я&nbsp;крутой дядя. Уже не&nbsp;молод, но всё еще понимаю эти ваши мемы.
-					Умею жать на&nbsp;клавиши так, чтобы ваши задумки, даже не&nbsp;самые приятные, мог увидеть весь мир. Даже против его воли.</p>
-				<a className="student__github-link"></a>
+			<div className="student__container">
+				<div className="student__info">
+					<h3 className="student__title">Никита</h3>
+					<p className="student__subtitle">Фронтенд-разработчик, 30&nbsp;лет</p>
+					<p className="student__about">Я&nbsp;крутой дядя. Уже не&nbsp;молод, но всё еще понимаю эти ваши мемы.
+						Умею жать на&nbsp;клавиши так, чтобы ваши задумки, даже не&nbsp;самые приятные, мог увидеть весь мир. Даже против его воли.</p>
+				</div>
+				<a
+					className="student__github-link"
+					href="https://github.com/Racio-begin"
+					target="_blank" rel="noreferrer"
+				>Github</a>
 			</div>
 			<img
 				className="student__avatar"
