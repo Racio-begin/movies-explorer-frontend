bdc79588f8e1e27f335f1f953b7585ee36f29324
Nikita Averyanov <rac1onal@yandex.ru>
2023-08-23 20:19:13 +0300

    feat: полностью сверстать компонент AboutProjects

--- src/components/AboutProjects/AboutProjects.js
+++ src/components/AboutProjects/AboutProjects.js
@@ -10,13 +10,13 @@ function AboutProjects() {
 			<div className="about-projects__description">
 
 				<div className="about-projects__description_container">
-					<div className="about-projects__description_title">Дипломный проект включал 5&nbsp;этапов</div>
-					<div className="about-projects__description_content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</div>
+					<h3 className="about-projects__description_title">Дипломный проект включал 5&nbsp;этапов</h3>
+					<p className="about-projects__description_content">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и&nbsp;финальные доработки.</p>
 				</div>
 
 				<div className="about-projects__description_container">
-					<div className="about-projects__description_title">На&nbsp;выполнение диплома ушло 5&nbsp;недель</div>
-					<div className="about-projects__description_content">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</div>
+					<h3 className="about-projects__description_title">На&nbsp;выполнение диплома ушло 5&nbsp;недель</h3>
+					<p className="about-projects__description_content">У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
 				</div>
 
 			</div>
