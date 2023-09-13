c890b53144769f7307418089a55ac6d8e3c582cf
Nikita Averyanov <rac1onal@yandex.ru>
2023-09-13 03:42:11 +0300

    refactor:  доработки после починки api

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,13 +1,10 @@
 //*	API-константы	*//
 
 // API-адрес для получения данных пользователя с моего сервера (локальная машина, тестирование)
-export const BASE_URL = "http://localhost:3000"
-
-// API-адрес для получения данных , тестирование
-// export const BASE_URL = "https://api.mattino13-movies-explorer.nomoredomains.rocks"
+// export const BASE_URL = "http://localhost:3000"
 
 // API-адрес для получения данных пользователя с моего сервера
-// export const BASE_URL = "https://api.giga-movies.nomoreparties.co";
+export const BASE_URL = "https://api.giga-movies.nomoreparties.co";
 
 // API-адрес для получения фильмов
 export const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
